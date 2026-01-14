c41f42fa0d6416175652a9e6f58384dad5ce0676
elifyyalcin <elifyyyalcin@gmail.com>
2026-01-14 16:18:46 +0300

    Initial Commit

---
+++ EnUygunCaseIOS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  EnUygunCaseIOS
+//
+//  Created by Elif Alaboyun on 14.01.2026.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ EnUygunCaseIOSTests/EnUygunCaseIOSTests.swift
@@ -0,0 +1,17 @@
+//
+//  EnUygunCaseIOSTests.swift
+//  EnUygunCaseIOSTests
+//
+//  Created by Elif Alaboyun on 14.01.2026.
+//
+
+import Testing
+@testable import EnUygunCaseIOS
+
+struct EnUygunCaseIOSTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
